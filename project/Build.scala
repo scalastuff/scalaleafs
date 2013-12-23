3d488ffdbd31cf16463a17fda5f96f448b21e6a9
Ruud Diterwich <ruud@diterwich.com>
2013-12-23 18:43:33 +0100

    added gitattributes and got rid of compiler warning

--- project/Build.scala
+++ project/Build.scala
@@ -13,8 +13,8 @@ object ScalaLeafsBuild extends Build {
         organization := "com.kentivo",
         version := "1.1.0-SNAPSHOT",
         scalaVersion := "2.10.3",
-        scalacOptions += "-deprecation",
-        scalacOptions += "-unchecked",
+        scalacOptions ++= Seq("-deprecation", "-unchecked", "-encoding", "utf8", "-feature"),
+        scalacOptions ++= Seq("-language:implicitConversions", "-language:postfixOps", "-language:reflectiveCall", "-language:higherKinds", "-language:existentials", "-language:reflectiveCalls"),
         EclipseKeys.createSrc := EclipseCreateSrc.Default + EclipseCreateSrc.Resource,
         EclipseKeys.withSource := true)
           
@@ -47,8 +47,8 @@ object ScalaLeafsBuild extends Build {
     libraryDependencies +=  "org.clapper" %% "grizzled-slf4j" % "1.0.1" withSources(),
     libraryDependencies +=  "nu.validator.htmlparser" % "htmlparser" % "1.4",
     libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.2.3" withSources(),
-    libraryDependencies += "io.spray" % "spray-can" % "1.2-RC2" withSources(),
-    libraryDependencies += "io.spray" % "spray-routing" % "1.2-RC2" withSources(),
+    libraryDependencies += "io.spray" % "spray-can" % "1.2.0" withSources(),
+    libraryDependencies += "io.spray" % "spray-routing" % "1.2.0" withSources(),
     libraryDependencies += "org.scalatest" %% "scalatest" % "2.0" % "test" withSources(),
     resolvers += "spray repo" at "http://repo.spray.io"))
 
